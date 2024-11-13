5c3b7ed810ff460842c3e9afa3aeb5472993d24c
AmineMekki01 <amine.mekki.contact@gmail.com>
2024-11-13 22:29:55 +0100

    feat: new logos

--- src/components/TechnicalStack.js
+++ src/components/TechnicalStack.js
@@ -1,67 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import {
-  DiPython, DiJavascript1, DiReact, DiDocker, DiAws
-} from 'react-icons/di';
-import {
-  SiPytorch, SiScikitlearn, SiFastapi, SiKubernetes,
-  SiPostgresql, SiMongodb, SiRedis, SiApachespark, SiApachekafka, SiMlflow, SiDvc
-} from 'react-icons/si';
 import techStackData from '../data/techStack.json';
 import { useTranslation } from 'react-i18next';
 
-const PineconeIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 2L15 8H9L12 2Z" fill="#64FFDA"/>
-    <path d="M12 22L9 16H15L12 22Z" fill="#64FFDA"/>
-    <path d="M2 12L8 9V15L2 12Z" fill="#64FFDA"/>
-    <path d="M22 12L16 15V9L22 12Z" fill="#64FFDA"/>
-  </svg>
-);
-
-
-const HuggingFaceIcon = styled.div`
-  svg {
-    width: 30px;
-    height: 30px;
-    color: #64FFDA;
-  }
-`;
-
-
-const iconMap = {
-  DiPython: DiPython,
-  DiJavascript1: DiJavascript1,
-  DiReact: DiReact,
-  DiDocker: DiDocker,
-  DiAws: DiAws,
-  SiPytorch: SiPytorch,
-  SiScikitlearn: SiScikitlearn,
-  SiFastapi: SiFastapi,
-  SiKubernetes: SiKubernetes,
-  SiPostgresql: SiPostgresql,
-  SiMongodb: SiMongodb,
-  SiRedis: SiRedis,
-  SiApachespark: SiApachespark,
-  SiApachekafka: SiApachekafka,
-  PineconeIcon: PineconeIcon,
-  SiMlflow: SiMlflow,
-  SiDvc: SiDvc,
-  HuggingFaceIcon: () => (
-    <HuggingFaceIcon>
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="12" cy="12" r="10" stroke="#64FFDA" strokeWidth="2"/>
-        <circle cx="9" cy="10" r="1" fill="#64FFDA"/>
-        <circle cx="15" cy="10" r="1" fill="#64FFDA"/>
-        <path d="M9 15H15" stroke="#64FFDA" strokeWidth="2"/>
-      </svg>
-    </HuggingFaceIcon>
-  )
-};
-
 const TechnicalStack = () => {
   const [techStack, setTechStack] = useState([]);
-  const {t } = useTranslation();
+  const { t } = useTranslation();
 
   useEffect(() => {
     setTechStack(techStackData);
@@ -71,15 +15,14 @@ const TechnicalStack = () => {
     <TechnicalStackContainer id="technical-stack">
       <Title>{t("technicalStack.title")}</Title>
       <StackList>
-        {techStack.map((item, index) => {
-          const IconComponent = iconMap[item.icon];
-          return (
-            <StackItem key={index}>
-              <IconComponent />
-              <StackItemText>{item.name}</StackItemText>
-            </StackItem>
-          );
-        })}
+        {techStack.map((item, index) => (
+          <StackItem key={index}>
+            <IconWrapper>
+              <img src={item.icon} alt={item.name} />
+            </IconWrapper>
+            <StackItemText>{item.name}</StackItemText>
+          </StackItem>
+        ))}
       </StackList>
     </TechnicalStackContainer>
   );
@@ -136,11 +79,19 @@ const StackItem = styled.div`
     transform: translateY(-10px);
     box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
   }
+`;
 
-  svg {
-    color: #64FFDA;
-    font-size: 3rem;
-    margin-bottom: 0.5rem;
+const IconWrapper = styled.div`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
   }
 `;
 
